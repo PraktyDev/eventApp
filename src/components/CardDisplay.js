d9dad3efc33992f84e2f6b8f11c4c3b16ed5e54d
Praise Akintayo <prakty1205@gmail.com>
2024-06-10 06:57:34 -0700

    fixed card display component  dynamic routing error on home page

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -37,7 +37,7 @@ export default function CardDisplay() {
     <div className="hidden basis-3/4 tablet:grid grid-cols-3 gap-4 my-4">
     {venue.map(item =>(      
         <div key={item._id} className="flex flex-col -gap-1">
-        <Link href={`events/${item._id}`}>
+        <Link href={`venues/${item._id}`}>
             <Image width={16} height={9} alt="card" src={item.image} className="object-cover rounded-lg bg-slate-600 w-40 tablet:w-40 h-32 tablet:h-40" />
             <h2 className='font-semibold text-sm tablet:text-md text-nowrap'>{item.name}</h2>
         </Link>
